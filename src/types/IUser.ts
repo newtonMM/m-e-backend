5ff055bfda38ada0f82be80ab5057d236a1bfab4
Dennis Macharia <dennismacharia4747@gmail.com>
2023-01-15 16:10:39 +0300

    refactor

---
+++ src/types/IUser.ts
@@ -0,0 +1,12 @@
+/**
+ * User interface
+ */
+export interface IUser {
+  email: string;
+  name: string;
+  password: string;
+  phoneNo: string;
+  idNo: string;
+  role: string;
+  fullName?: string;
+}
